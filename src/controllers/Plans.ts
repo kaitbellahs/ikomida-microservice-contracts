4f3abea2c8435132801f10a73aa7cb13a0d81b7c
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-10-26 10:22:46 -0300

    Add plan dueDateAfterXDays

--- src/controllers/Plans.ts
+++ src/controllers/Plans.ts
@@ -33,10 +33,11 @@ export default class Plans {
           planModel.support ?? [],
           planModel.highlighted ?? false,
           (planModel?.price ?? 0) -
-            Logics.Finances.calcDiscount(planModel?.price ?? 0, planModel?.discount ?? 0, planModel?.discountType),
+          Logics.Finances.calcDiscount(planModel?.price ?? 0, planModel?.discount ?? 0, planModel?.discountType),
           undefined,
           undefined,
           planModel.order,
+          planModel.dueDateAfterXDays,
           planModel.id
         )
         return plan
